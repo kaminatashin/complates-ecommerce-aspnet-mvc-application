db9ba7bf539a882378966eeb2c771f781bce6aa9
kaminatashin <atashin@ATASHIN-PC>
2024-08-05 13:16:54 +0330

    Office14030515_13

---
+++ eTickets/Data/AppDbInitializer.cs
@@ -0,0 +1,95 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                context.Database.EnsureCreated();
+                //Cinema
+
+                if (!context.Cinemas.Any())
+                {
+                    context.Cinemas.AddRange(new List<Cinema>
+                    {
+                        new Cinema()
+                        {
+                            Name="Cinema  1",
+                            logo="http://dotnethow.net/images/cinemas/cinema-1.jpeg",
+                            Description="This is the description of teh first cinema"
+                        },
+                        new Cinema()
+                        {
+                            Name="Cinema  2",
+                            logo="http://dotnethow.net/images/cinemas/cinema-2.jpeg",
+                            Description="This is the description of teh first cinema"
+                        },
+                        new Cinema()
+                        {
+                            Name="Cinema  3",
+                            logo="http://dotnethow.net/images/cinemas/cinema-3.jpeg",
+                            Description="This is the description of teh first cinema"
+                        },
+                        new Cinema()
+                        {
+                            Name="Cinema  4",
+                            logo="http://dotnethow.net/images/cinemas/cinema-4.jpeg",
+                            Description="This is the description of teh first cinema"
+                        },
+                       new Cinema()
+                        {
+                            Name="Cinema  5",
+                            logo="http://dotnethow.net/images/cinemas/cinema-5.jpeg",
+                            Description="This is the description of teh first cinema"
+                        },
+                    });
+                    context.SaveChanges(); 
+
+                }
+                //Actors
+                if (!context.Actors.Any())
+                {
+                    context.Actors.AddRange(new List<Actor>() {
+
+                        new Actor() {
+                            
+                        }
+
+
+
+
+
+                    });
+                                     
+                        
+                        
+                       
+
+                }
+
+                //Produsers
+                if (!context.Producers.Any()) 
+                { 
+                
+                
+                }
+                //Movies
+                if(!context.Movies.Any())
+                {
+
+
+                }
+                //Actors & Movies
+                if(!context.Actor_Movies.Any())
+                {
+
+                }
+            }
+             
+        }
+    }
+}
--- eTickets/Data/Enums/MovieCategory.cs
+++ eTickets/Data/Enums/MovieCategory.cs
@@ -5,7 +5,8 @@
         Action = 1,
         Comedy,
         Drama,
-        Documentary
+        Documentary,
+        Carton
 
     }
 }
